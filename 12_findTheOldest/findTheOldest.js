6dffe7258a92c2e44ab772c72e8ca0d958adcc6d
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-08-10 18:39:00 +0100

    Complete JavaScript Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  array.sort((firstPerson, nextPerson) => {
+    let firstAge;
+    let nextAge;
 
+    if (firstPerson.yearOfDeath === undefined) {
+      firstAge = new Date().getFullYear() - firstPerson.yearOfBirth;
+    } else {
+      firstAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+    }
+
+    if (nextPerson.yearOfDeath === undefined) {
+      nextAge = new Date().getFullYear() - nextPerson.yearOfBirth;
+    } else {
+      nextAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+    }
+    return firstAge > nextAge ? -1 : 1;
+  });
+
+  return array[0];
 };
 
 // Do not edit below this line
