75c927b5a1340d302b2c9682d859882f0bfe1ecf
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-08-10 11:53:55 +0100

    Complete JavaScript Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elements) {
+  elements.forEach((element) => {
+    element = array.indexOf(element);
 
+    if (element >= 0) {
+      array.splice(element, 1);
+    }
+  });
+
+  return array;
 };
 
 // Do not edit below this line
