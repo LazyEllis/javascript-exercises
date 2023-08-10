78d8cdb28bca19f5f96903dbef8e9b3f69be8e91
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-08-10 16:34:37 +0100

    Complete JavaScript Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let sequence = [];
 
+  if (number < 0) {
+    return 'OOPS';
+  } else {
+    for (let i = 0; i < number; i++) {
+      if (i < 2) {
+        sequence.splice(i, 0, 1);
+      } else {
+        sequence.splice(i, 0, sequence[i - 1] + sequence[i - 2]);
+      }
+    }
+  }
+
+  return sequence[number - 1];
 };
 
 // Do not edit below this line
