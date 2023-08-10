e6941508d2fec33d3523e117bd557130fc171cee
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-08-10 15:45:00 +0100

    Complete JavaScript Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, lastNumber) {
+  let sum = 0;
+  let fNum;
+  let lNum;
 
+  if (
+    firstNumber < 0 ||
+    lastNumber < 0 ||
+    typeof firstNumber !== 'number' ||
+    typeof lastNumber !== 'number'
+  ) {
+    return 'ERROR';
+  } else if (firstNumber >= lastNumber) {
+    fNum = lastNumber;
+    lNum = firstNumber;
+  } else if (firstNumber < lastNumber) {
+    fNum = firstNumber;
+    lNum = lastNumber;
+  }
+
+  for (let i = fNum; i <= lNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
